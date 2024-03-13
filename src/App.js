8131f17c0043dc53d2a17a8ea3a3e991a1b883e2
Divakar Kumar Gupta <131536312+lonesoul1@users.noreply.github.com>
2024-03-13 23:41:00 +0530

    changes done

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useCallback } from "react";
+
+import Counter from "./components/Counter";
+import CounterButton from "./components/CounterButton";
+import SetCounter from "./components/SetCounter";
+
+function App() {
+  console.log("APP RENDERED");
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+
+  const handleDecrement = useCallback(() => {
+    setCount((prev) => prev - 1);
+  }, []);
+
+  const handleSetCount = (newCount) => {
+    setCount(newCount);
+  };
+
+  return (
+    <div>
+      <Counter count={count} />
+      <CounterButton onButtonClick={handleIncrement}>Increase</CounterButton>
+      <CounterButton onButtonClick={handleDecrement}>Decrease</CounterButton>
+      <SetCounter onSet={handleSetCount} />
+
+      {/* <SetCounter onSet={handleSetCount} /> */}
+    </div>
+  );
+}
+
+export default App;
